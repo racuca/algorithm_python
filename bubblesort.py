49f5f70c64900ff3d189ca62f4a1bdb980935d9e
Rockiahn <racuca@gmail.com>
2024-02-20 13:11:11 +0900

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,16 +1,15 @@
-def bubblesort(datalist):
-    for idx in range(len(datalist) - 1):
-        for index in range(1, len(datalist) - idx):
-            if datalist[index - 1] > datalist[index]:
-                temp = datalist[index - 1]
-                datalist[index - 1] = datalist[index]
-                datalist[index] = temp
-                print("index", index - 1, index, "data 교환", datalist[index - 1], datalist[index])
+def bubblesort(data):
+    for sorted_num in range(len(data)-1):
+        for idx in range(len(data)-1-sorted_num):
+            if data[idx] > data[idx+1]:
+                temp = data[idx]
+                data[idx] = data[idx+1]
+                data[idx+1] = temp
+                print("index", idx, idx+1, "data교환", data[idx], data[idx+1])
             else:
-                print("index", index - 1, index)
-        print("턴 정렬 결과", datalist)
+                print("index", idx, idx+1)
 
-list = [3, 4, 5, 1, 2]
-print("정렬 전 =>", list)
-bubblesort(list)
-print("정렬 결과 =>", list)
+l1 = [3, 4, 5, 1, 2]
+print("데이터", l1)
+bubblesort(l1)
+print("정렬 데이터", l1)
